f611ab020ed6fa4a1bc9c3c0691db3cd0d97ca18
Harshul Malik <harshulmalik5@gmail.com>
2025-01-18 17:01:44 +0530

    variables.js

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 2892
+let accountEmail = "fhj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState
+
+accountEmail = "dcj@.com"
+accountPassword = "213741"
+accountCity = "delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
